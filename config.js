e4d7abc3a17bd38bacb1ea4fba3f361b7ebb4f4c
baothanhh <88760573+baothanhh@users.noreply.github.com>
2021-08-11 12:52:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ const CONFIG = {
     desc: 'nếu b không trả lời mà thoát ra tức là muốn làm vợ mình rùi đó nha :3 ',
     btnYes: 'iu b nhìu lắm lunn<3',
     btnNo: 'b mơ àaa',
-    question:'trên thế giới hơn 7 tỉ người mà sao b lại yêu mình <3',
+    question:'trên thế giới hơn 7 tỉ người mà sao b lại iu mình <3',
     btnReply: 'gửi cho anh <3',
     reply: 'iu thì iu mà không iu thì iu <33333333',
     mess: 'mình biết mà 🥰. iu b nhìu nhìu 😘😘',
-    messDesc: 'hôm nào mình mua tsua cho b nhaaaaa',
+    messDesc: 'giờ b chịu đau xíuu. hôm nào mình mua tsua cho b nhaaaaa',
     btnAccept: 'okiiiii lun <3',
     messLink: 'http://fb.com' 
 }    
